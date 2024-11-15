d3d7fa97bd170f117633b65e4774c93515dba2c2
a110959 <a110959@alunos.uminho.pt>
2024-11-15 11:06:29 +0000

    modified Ficha6.hs

--- Ficha6.hs
+++ Ficha6.hs
@@ -4,9 +4,11 @@ data BTree a = Empty
              | Node a (BTree a) (BTree a)
              deriving Show
 
-testBTree = Node 5 (Node 3 Empty Empty)
+testBTree = Node 5 (Node 3 Empty 
+                           (Node 4 Empty Empty))
                    (Node 7 (Node 6 Empty Empty)
-                           (Node 9 (Node 8 Empty Empty) Empty))
+                           (Node 9 (Node 8 Empty Empty)
+                                   Empty))
 
 altura :: BTree a -> Int
 altura Empty = 0
@@ -33,8 +35,25 @@ path (h:t) (Node a l r) | h         = a : path t r
                         | otherwise = a : path t l
 
 mirror :: BTree a -> BTree a
-mirror 
+mirror Empty = Empty
+mirror (Node a l r) = Node a (mirror r) (mirror l)
 
+zipWithBT :: (a -> b -> c) -> BTree a -> BTree b -> BTree c
+zipWithBT f (Node a1 l1 r1) (Node a2 l2 r2) = Node (f a1 a2) (zipWithBT f l1 l2) (zipWithBT f r1 r2)
+zipWithBT _ _ _ = Empty
 
+-- Exercício 2
+minimo :: Ord a => BTree a -> a
+minimo (Node a Empty _) = a
+minimo (Node a l _) = minimo l
+
+semMinimo :: Ord a => BTree a -> BTree a
+semMinimo (Node a Empty r) = r
+semMinimo (Node a l r) = Node a (semMinimo l) r
+
+minSmin :: Ord a => BTree a -> (a, BTree a)
+minSmin (Node a Empty r) = (a, r)
+minSmin (Node a l r) = (min, Node a tree r)
+                     where (min, tree) = minSmin l
 
 -- Exercício Extra: Converter lista de inteiros em BTree de inteiros.
